2e1b60d64a68dc9183d4730a005e8bfd09e2fcbe
jcdominguez <jcd.pro@gmail.com>
2022-12-19 14:31:11 +0100

    Ajout WS PUT /clients/id

--- src/main/java/com/poe20221107/crm/api/CrmApi.java
+++ src/main/java/com/poe20221107/crm/api/CrmApi.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,5 +42,14 @@ public class CrmApi {
     @Path("/{id}")
     public void deleteClient(@PathParam("id") Long id){
         ClientDAO.deleteById(id);
+    }
+    
+    @PUT()
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void updateClient(Client client, @PathParam("id") Long id){
+        // TODO : vérifier si id dans URL est identique à id dans body
+        
+        ClientDAO.update(client);
     }
 }
